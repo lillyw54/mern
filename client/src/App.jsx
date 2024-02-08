697a68581254e2c8318665e75c9c053f4707fb75
idilia wheat <lillyw54@gmail.com>
2024-02-07 18:39:55 -0600

    login and navbar updates

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import './App.css';
+import React from 'react';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import { Outlet } from 'react-router-dom';
-
 import Navbar from './components/Navbar';
+
+// Create an Apollo Client instance
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Replace with your GraphQL server endpoint
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    // Wrap your entire application with ApolloProvider and pass in the client
+    <ApolloProvider client={client}>
+      <>
+        <Navbar />
+        <Outlet />
+      </>
+    </ApolloProvider>
   );
 }
 
